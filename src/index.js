0e22e53fc2aebded02470dccadf9f1b8aadd367e
Elena Vlasenko <olefelena@gmail.com>
2021-10-13 16:05:49 +0300

    completed new tasks

---
+++ src/index.js
@@ -0,0 +1,4 @@
+
+// import SumOfOddNumbers from './js/7kyu/sum_of odd numbers'
+
+import money from './js/7kyu/Mr. Scrooge'
---
+++ src/js/7kyu/sum_of odd numbers.js
@@ -0,0 +1,21 @@
+/* Given the triangle of consecutive odd numbers:
+             1
+          3     5
+       7     9    11
+   13    15    17    19
+21    23    25    27    29
+...
+
+Calculate the sum of the numbers in the n-th row of this triangle 
+(starting at index 1) e.g.: (Input --> Output) 
+1 -->  1
+2 --> 3 + 5 = 8 */
+export default function rowSumOddNumbers(n) {
+	return Math.pow(n, 3);
+}
+
+console.log(rowSumOddNumbers(1));
+console.log(rowSumOddNumbers(2));
+console.log(rowSumOddNumbers(3));
+console.log(rowSumOddNumbers(4));
+console.log(rowSumOddNumbers(5));
