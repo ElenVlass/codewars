79f3fdff0d408a0289d7a507296d6275f2691b01
Elena Vlasenko <olefelena@gmail.com>
2021-10-13 16:11:42 +0300

    added example

--- src/js/7kyu/Mr. Scrooge.js
+++ src/js/7kyu/Mr. Scrooge.js
@@ -15,4 +15,20 @@ export default function calculateYears(principal, interest, tax, desired) {
      if(sum>=desired) return year;
  }
 }
- console.log(calculateYears(1000, 0.05, 0.18, 1100) );
+
+ console.log(calculateYears(1000, 0.05, 0.18, 1100) ); // 3;
+ 
+ /* Example:
+
+  Let P be the Principal = 1000.00      
+  Let I be the Interest Rate = 0.05      
+  Let T be the Tax Rate = 0.18      
+  Let D be the Desired Sum = 1100.00
+
+
+After 1st Year -->
+  P = 1041.00
+After 2nd Year -->
+  P = 1083.86
+After 3rd Year -->
+  P = 1128.30 */
